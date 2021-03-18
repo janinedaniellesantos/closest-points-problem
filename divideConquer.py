e8ea509cd4c9935fe558f49953394d7efb9ca4e3
janinedaniellesantos <59384505+janinedaniellesantos@users.noreply.github.com>
2021-03-18 16:03:25 -0700

    Update divideConquer.py

--- divideConquer.py
+++ divideConquer.py
@@ -1,43 +1,72 @@
-def divideAndConquer(points, c, d, distance):
-    length = len(points)
-    
+import math
+
+#calculates the ditance between points 
+def elucidian(a,b) :
+    return math.sqrt(math.pow((a[0] - b[0]), 2) + math.pow((a[1]-b[1]), 2)) 
+
+#brute force approach 
+def bruteForce(coord,pointOne,pointTwo,eluDist) :
+    length = len(coord)
+    if length == 2 :
+        dist = elucidian(coord[0],coord[1])
+        if dist < eluDist :
+            pointOne,pointTwo = coord[0],coord[1]
+            eluDist = dist
+    else: 
+        for i in (0, length-1) :
+            for j in range(i + 1, length) : 
+                distance = elucidian(points[i], points[j]) 
+                if distance < eluDist : 
+                    distance = eluDist
+                    pointOne = points[i]
+                    pointTwo = points[j]
+    return pointOne, pointTwo, eluDist
+
+#array length <= 3 use brute force approach
+def divideConquer(coord,pointOne,pointTwo,eluDist) :
+    length = len(coord) 
+    if length <= (3) : 
+        return (bruteForce(coord,pointOne,pointTwo,eluDist)) 
+
     #dividing the list into 2
-    half = length/2
+    half = length//2
 
     #seperation line so that half of the points are on either side and vice versa
-    middle = (points[half])
+    middle = coord[half][0]
+    leftX = coord[:half]
+    rightX = coord[half:]
 
-    left = points[:middle]
-    right = points[middle:]
+    pointOne,pointTwo,eluDist = closestPairs(leftX,pointOne,pointTwo,eluDist)
+    pointOne,pointTwo,eluDist = closestPairs(rightX,pointOne,pointTwo,eluDist)
+    pointOne,pointTwo,eluDist = resultsDC(coord,middle,pointOne,pointTwo,eluDist)
+	
+    return (pointOne,pointTwo,eluDist)
 
-    c,d,distance = divideAndConquer(left,c,d,distance)
-    c,d,distance = divideAndConquer(right,c,d,distance)
-    c,d,distance = resultsDC(points,middle,c,d,distance)
+def resultsDC(coord,midPoint,pointOne,pointTwo,eluDist):
+    resultsDC = [] 
+    length = len(resultsDC)
+    r,l = midPoint+int(eluDist), midPoint-int(eluDist)
 
-    return(c,d,distance)
-
-def resultsDC(points,midPoint,c,d,distance):
-    resultsDC = []
-    length = len(points)
-    #half = int (length/2)
-    midPoint = points[half]
-
-    r,l = midPoint + int(distance), midPoint-int(distance)
-    #for every point in points, find the shortest distance and append those to 
-    for i in range (0, len(points)) : 
-        if i[0]>r :
+    #for every point in points, find the shortest distance and append those to array
+    for x in coord:
+        if x[0] > r: 
             break
-        if l <= x[0] <= r:
-            resultsDC.append(i)
-    
-    resultsDC.sort(key=lambda i:i[1])
+        elif l <=x [0] <= r: resultsDC.append(x)
 
-    for i in range (0, len(points)):
-        min = elucidian(resultsDC[x], resultsDC[y])
-        if min < distance: 
-            c,d = resultsDC[x], resultsDC[y]
-            distance = min
-    return (c,d, distance)
+    sY.sort(key = lambda x:x[1]) 
 
-#pointOne, pointTwo, dist = divideAndConquer(points, c, d, distance)
-#print("DNC: The closest pair of points are", pointOne, pointTwo, dist)
+    for x in range (0,length):
+        for y in range (x+1, min ((x+7),length)) :
+            dist = elucidian(resultsDC[x],resultsDC[y])
+            if dist < eluDist :
+                pointOne,pointTwo=resultsDC[x],resultsDC[y]
+                eluDist = dist           
+    return (pointOne,pointTwo,eluDist)
+
+points = [(1,1), (1,2), (10,20), (1,100), (5,100), (6,6), (7,8), (20,13), (21,44), (112,11), (11,33), (21, 20), (17, 90) , (33,98)] 
+points.sort()
+pointOne, pointTwo = points[0],points[1] 
+eluDist=elucidian (points[0],points[1]) 
+cPOne,cPTwo,distance = bruteForce(points,pointOne,pointTwo,eluDist)
+#print
+print ("Divide and Conquer Approach: The closest pair of points from the list are", cPOne, cPTwo)
