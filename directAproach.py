38137be5cecef739c0ff43f742f5951e15bc640f
janinedaniellesantos <59384505+janinedaniellesantos@users.noreply.github.com>
2021-03-18 16:06:23 -0700

    cleaned up comments still unable to get input from file

--- directAproach.py
+++ directAproach.py
@@ -2,15 +2,6 @@ import sys
 import math
 import copy
 
-#points = []
-#pointsList = open(sys.argv[1], "r")
-
-#for z in pointsList :
-    #z = z.strip()
-    #x,y = z.split(' ')
-    #points.append(x,y)
-
-#finding the distance between two points 
 def elucidian(a,b) :
     return math.sqrt(math.pow((a[0] - b[0]), 2) + math.pow((a[1]-b[1]), 2)) 
 
@@ -30,13 +21,12 @@ def bruteForce(points) :
 
 points = [(1,1), (1,2), (10,20), (1,100), (5,100), (6,6), (7,8), (20,13), (21,44), (112,11)] 
 points.sort()
-print(points)
 c = points[0]
 d = points[1]
 distance = elucidian(points[0], points[1])
 
 bruteForceAnswer = bruteForce(points)
-print("Brute Force Method: The closest pair of points from the list are", bruteForceAnswer)
+print("Brute Force Approach: The closest pair of points from the list are", bruteForceAnswer)
 
 
 
